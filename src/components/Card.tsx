1c4cee83a289649f9cb3177d7669bc1343716a66
dudupt1 <boysigma111111@gmail.com>
2026-07-12 16:55:08 +0100

    Atualizar o Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,1 +1,27 @@
- 
+import { ReactNode } from "react";
+
+type CardProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+export default function Card({
+  children,
+  className = "",
+}: CardProps) {
+  return (
+    <div
+      className={`
+        bg-zinc-900
+        border
+        border-zinc-800
+        rounded-2xl
+        p-6
+        shadow-lg
+        ${className}
+      `}
+    >
+      {children}
+    </div>
+  );
+} 
