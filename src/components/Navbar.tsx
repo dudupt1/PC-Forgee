2ef666673e8196b4b46a419a3c2b0e351c7c2f86
dudupt1 <boysigma111111@gmail.com>
2026-07-12 16:56:30 +0100

    Atualizar o Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,56 @@
- 
+import { useNavigate } from "react-router-dom";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <nav
+      className="
+        w-full
+        bg-zinc-950
+        border-b
+        border-zinc-800
+        px-8
+        py-4
+        flex
+        justify-between
+        items-center
+        text-white
+      "
+    >
+
+      <button
+        onClick={() => navigate("/")}
+        className="
+          text-2xl
+          font-bold
+          hover:text-blue-500
+          transition
+        "
+      >
+        ⚒️ PC Forge
+      </button>
+
+
+      <div className="flex gap-6">
+
+        <button
+          onClick={() => navigate("/favorites")}
+          className="hover:text-blue-500 transition"
+        >
+          ❤️ Favoritos
+        </button>
+
+
+        <button
+          onClick={() => navigate("/settings")}
+          className="hover:text-blue-500 transition"
+        >
+          ⚙️ Definições
+        </button>
+
+      </div>
+
+    </nav>
+  );
+} 
