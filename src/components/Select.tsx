9d34035d0d5b568c0d7f109724697cdd0366a974
dudupt1 <boysigma111111@gmail.com>
2026-07-12 16:55:55 +0100

    Atualizar o Select.tsx

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,1 +1,51 @@
- 
+ type Option = {
+  label: string;
+  value: string;
+};
+
+type SelectProps = {
+  options: Option[];
+  value?: string;
+  onChange?: (value: string) => void;
+  placeholder?: string;
+};
+
+export default function Select({
+  options,
+  value = "",
+  onChange,
+  placeholder = "Selecionar...",
+}: SelectProps) {
+  return (
+    <select
+      value={value}
+      onChange={(e) => onChange?.(e.target.value)}
+      className="
+        w-full
+        bg-zinc-900
+        border
+        border-zinc-800
+        rounded-xl
+        px-4
+        py-3
+        text-white
+        outline-none
+        focus:border-blue-600
+        transition
+      "
+    >
+      <option value="">
+        {placeholder}
+      </option>
+
+      {options.map((option) => (
+        <option
+          key={option.value}
+          value={option.value}
+        >
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+}
